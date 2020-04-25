756e107dd7360c111cb6b60d00e9c3b31feab20a
Dave Campbell <dave@digetl.co.uk>
2020-04-25 17:55:10 +0100

    Added update crUd to books

--- models/book.rb
+++ models/book.rb
@@ -53,7 +53,26 @@ class Book
         values = ['id']
         SqlRunner.run(sql,values)
     end
-    
+
+    def update()
+    sql = "UPDATE books
+    SET
+    (
+            title,
+            book_type,
+            stock_level,
+            selling_price,
+            buying_price,
+            author_id)
+        VALUES
+        (
+            $1, $2, $3, $4, $5, $6
+        )
+        WHERE id = $7"
+        values = [@title, @book_type, @stock_level, @selling_price, @buying_price, @author_id, @id]
+        SqlRunner.run( sql, values )
+    end
+
 
 
 end
